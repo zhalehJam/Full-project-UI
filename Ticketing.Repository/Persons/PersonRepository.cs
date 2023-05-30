e67d8b179e64db7b6828611a502fa2e5c304a653
970086 <970086@JAMALIVAND>
2023-05-30 14:13:25 +0330

    Commit Ticket syncfusion Grid- Change Repository

--- Ticketing.Repository/Persons/PersonRepository.cs
+++ Ticketing.Repository/Persons/PersonRepository.cs
@@ -46,17 +46,16 @@ namespace Ticketing.Repository.Persons
 
             return personDtos;
         }
-
-        private static List<PersonDto> GetPersonDtoFromContent(string content)
+        public async Task<PersonDto> GetPersonInfoByPersonelCode(int PersonnelCode)
         {
-            List<PersonDto> personDtos = new List<PersonDto>();
-            JArray jsonResponse = JArray.Parse(content);
+            PersonDto personDtos = new PersonDto();
+            IDictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("personnelCode", PersonnelCode.ToString());
+            string request = QueryHelpers.AddQueryString("Person/GetPersonInfoByPersonelCode", parameters);
+            var response = await _httpClient.GetAsync(request);
+            var content = await response.Content.ReadAsStringAsync();
+            personDtos = JsonConvert.DeserializeObject<PersonDto>(content);
 
-            foreach(var item in jsonResponse)
-            {
-                PersonDto? dto = JsonConvert.DeserializeObject<PersonDto>(item.ToString());
-                personDtos.Add(dto);
-            }
             return personDtos;
         }
 
@@ -89,19 +88,6 @@ namespace Ticketing.Repository.Persons
             await SendRequest<DeletePersonCommand>(deletePersonCommand, HttpMethod.Delete, "Person/DeletePerson");
         }
 
-        public async Task<PersonDto> GetPersonInfoByPersonelCode(int PersonnelCode)
-        {
-             PersonDto  personDtos = new PersonDto();
-            IDictionary<string, string> parameters = new Dictionary<string, string>();
-            parameters.Add("personnelCode", PersonnelCode.ToString()); 
-            string request = QueryHelpers.AddQueryString("Person/GetAllPersonsByPage", parameters);
-            var response = await _httpClient.GetAsync(request);
-            var content = await response.Content.ReadAsStringAsync();
-            personDtos = GetPersonDtoFromContent(content).First();
-
-            return personDtos;
-        }
-
         private async Task SendRequest<T>(T command, HttpMethod httpMethod, string uri)
         {
             var postRequest = new HttpRequestMessage(httpMethod, uri)
@@ -120,5 +106,17 @@ namespace Ticketing.Repository.Persons
             }
         }
 
+        private List<PersonDto> GetPersonDtoFromContent(string content)
+        {
+            List<PersonDto> personDtos = new List<PersonDto>();
+            JArray jsonResponse = JArray.Parse(content);
+
+            foreach(var item in jsonResponse)
+            {
+                PersonDto? dto = JsonConvert.DeserializeObject<PersonDto>(item.ToString());
+                personDtos.Add(dto);
+            }
+            return personDtos;
+        }
     }
 }
