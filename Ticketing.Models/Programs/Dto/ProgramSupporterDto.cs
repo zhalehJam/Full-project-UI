521e547e705c91e4ca9e826192d47072c110f70b
970086 <970086@JAMALIVAND>
2023-06-03 10:04:29 +0330

    Commit ProgramList UI syncfusion viersion

--- Ticketing.Models/Programs/Dto/ProgramSupporterDto.cs
+++ Ticketing.Models/Programs/Dto/ProgramSupporterDto.cs
@@ -4,9 +4,9 @@
     {
         public Guid Id { get; set; }
         public Guid ProgramId { get; set; }
-        public string ProgramName { get; set; }
+        public string ProgramName { get; set; } = "";
         public int SupporterpersonID { get; set; }
-        public string SupporterName { get; set; }
+        public string SupporterName { get; set; } = "";
     }
 
 }
