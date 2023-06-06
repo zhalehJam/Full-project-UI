72a34db571bf4f4d9d0acf19ce12d021b1cc4faa
970086 <970086@JAMALIVAND>
2023-06-06 17:36:42 +0330

    Commit set supporter name by their id, change table row in program, set required field in ticket

--- Ticketing.Models/Tickets/Dto/TicketDto.cs
+++ Ticketing.Models/Tickets/Dto/TicketDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,17 @@ namespace Ticketing.Models.Tickets.Dto
     public class TicketDto
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "program required")]
         public int PersonID { get; set; }
+
         public string PersonName { get; set; }
         public Guid PersonPartId { get; set; }
         public Guid PersonCenterId { get; set; }
         public string PersonPartName { get; set; }
         public string PersonCenterName { get; set; }
+        [Required(ErrorMessage = "program required")]
         public Guid ProgramId { get; set; }
+        [Required(ErrorMessage = "program required")]
         public string ProgramName { get; set; }
         public int ErrorTypeid { get; set; }
         public string ErrorTypeName { get; set; }
@@ -24,7 +29,7 @@ namespace Ticketing.Models.Tickets.Dto
         public string TicketTypeName { get; set; }
         public string? ErrorDescription { get; set; }
         public string? SolutionDescription { get; set; }
-        public DateTime TicketTime { get; set; }=DateTime.Now;
+        public DateTime TicketTime { get; set; } = DateTime.Now;
         public int TicketConditionid { get; set; }
         public string TicketConditionTypeName { get; set; }
         public int SupporterPersonID { get; set; }
