c29573e3bf91583dc18d5f5437b7807b110ff45f
970086 <970086@JAMALIVAND>
2023-06-14 12:23:47 +0330

    Commit show menu based on user role

--- Ticketing.Models/Persons/Command/UpdatePersonCommand.cs
+++ Ticketing.Models/Persons/Command/UpdatePersonCommand.cs
@@ -1,9 +1,12 @@
-﻿namespace Ticketing.Models.Persons.Command
+﻿using Ticketing.Client.Contracts.Persons;
+
+namespace Ticketing.Models.Persons.Command
 {
     public class UpdatePersonCommand
     {
         public Guid Id { get; set; }
         public string? Name { get; set; } 
         public Guid PartId { get; set; }
+        public RoleType personRole { get; set; }
     }
 }
--- Ticketing.Models/Persons/Dto/PersonDto.cs
+++ Ticketing.Models/Persons/Dto/PersonDto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ticketing.Client.Contracts.Persons;
 
 namespace Ticketing.Models.Persons.Dto
 {
@@ -14,5 +15,6 @@ namespace Ticketing.Models.Persons.Dto
         public Guid PartId { get; set; }
         public string? PartName { get; set; } = "";
         public string? CenterName { get; set; } = "";
+        public RoleType PersonRole { get; set; }
     }
 }
